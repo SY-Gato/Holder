33f4f520c5dd51056882e3dec64f771fa91eb87f
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-04-05 11:30:21 -0500

    Update Try3.js

--- Release/Try3.js
+++ Release/Try3.js
@@ -1,1 +1,10 @@
-function goofyahh(params = {})
+class goofyahh {
+  constructor(name) {
+    this.name = name;
+  }
+  show() {
+    alert("Something: ${this.name}");
+  }
+}
+
+export default goofyahh;
